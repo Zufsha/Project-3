de645a412f560dddd6b3b8c7952f50d7867cde30
Zufsha <zkhan26@hawk.iit.edu>
2022-11-22 23:09:44 -0600

    debugging

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-// Initialize and add the map
+
 function initMap() {
-  // The location of Uluru
+
   const norway = { lat: 60.4720, lng: 8.4689 };
-  // The map, centered at Uluru
+
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
     center: norway,
   });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: norway,
+   google.maps.marker = new google.maps.Marker({
+    position: norway
+	position:new google.maps.Marker(
     map: map,
   });
 }
